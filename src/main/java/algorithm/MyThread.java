b1e67e257615b5337acd49f55c39d146043594a8
李玺康 <lixikang@iie.ac.cn>
2020-10-31 21:37:44 +0800

    修改代码

---
+++ src/main/java/algorithm/MyThread.java
@@ -0,0 +1,26 @@
+package algorithm;
+
+/**
+ * @ClassNameMyThread
+ * @Author lixkvip@126.com
+ * @Date2020/10/31 14:40
+ * @Version V1.0
+ **/
+public class MyThread extends Thread{
+    int ticket;
+    public MyThread(int ticket) {
+        this.ticket = ticket;
+    }
+
+
+
+    @Override
+    public void run() {
+        while (ticket > 0) {
+            String threadName = Thread.currentThread().getName();
+            System.out.println(threadName + "卖出了" + ticket);
+            ticket--;
+        }
+    }
+}
+
